2be739797d1502e3a2bdf5fde8122abf21ff3a5e
kirilnikolov <kiril.nikolov.97@gmail.com>
2019-10-13 10:19:22 +0300

    Add hardcoded authentication service

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
+import {HardcodedAuthenticationService} from '../service/hardcoded-authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -13,7 +14,8 @@ export class LoginComponent implements OnInit {
   errorMessage = 'Empty username or password!';
   invalidLogin = false;
 
-  constructor(private router: Router) {
+  constructor(private router: Router,
+              private hardcodedAuthenticationService: HardcodedAuthenticationService) {
   }
 
   ngOnInit() {
@@ -21,7 +23,7 @@ export class LoginComponent implements OnInit {
 
 
   handleLogin() {
-    if (this.username !== '' && this.password !== '') {
+    if (this.hardcodedAuthenticationService.authenticate(this.username, this.password)) {
       this.invalidLogin = false;
       this.router.navigate(['welcome/', this.username]);
     } else {
